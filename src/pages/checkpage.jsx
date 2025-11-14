6ce7f49d5c7e716d39ae079fb05c1686643728da
Shubham yadav <shubhamyadav1662000@gmail.com>
2025-11-14 16:52:34 +0530

    checkfile added to feature branch

---
+++ src/pages/checkpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const checkpage = () => {
+    return (
+        <div>
+            This is check page only for checking branchs features
+        </div>
+    )
+}
+
+export default checkpage
